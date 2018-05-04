92c3606089624d909a72975095cbe8738f7e08ed
Andy Tang <andytang247@gmail.com>
2018-05-04 13:03:03 +1200

    Create test for ImageDownloader

---
+++ src/main/images/test/ImageDownloaderTest.java
@@ -0,0 +1,14 @@
+package main.images.test;
+
+import org.junit.Test;
+
+import main.images.downloader.ImageDownloader;
+
+public class ImageDownloaderTest {
+	
+	@Test
+	public void testDownloadRecentPhotos() {
+		new ImageDownloader().downloadRecentImages();
+	}
+
+}
