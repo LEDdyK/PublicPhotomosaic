fd81dd6a5590da001cead3196129c69980dbba8b
Andy Tang <andytang247@gmail.com>
2018-05-04 13:03:11 +1200

    Add PhotoMetaData class

---
+++ src/main/images/downloader/PhotoMetaData.java
@@ -0,0 +1,13 @@
+package main.images.downloader;
+
+public class PhotoMetaData {
+	private static String id;
+	private static String secret;
+	private static int server;
+	private static int farm;
+	
+	public PhotoMetaData(String xmlLine) {
+		
+	}
+
+}
