ba9b899fcfeb40d2be857a9f6fbe9cad3560a8a1
Andy Tang <andytang247@gmail.com>
2018-05-11 15:15:44 +1200

    Set up paratask

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,41 +6,49 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import apt.annotations.Future;
+import apt.annotations.InitParaTask;
+import apt.annotations.TaskScheduingPolicy;
 import main.images.ImageGrid;
 import main.images.RGBLibrary;
 import main.images.downloader.ImageDownloader;
 import main.images.reader.ImageLibrary;
+import pt.runtime.ParaTask;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-	try {
-			long startTime = System.currentTimeMillis();
-			
-			new ImageDownloader().downloadRecentImages();
-			
-			ImageLibrary imglib = new ImageLibrary("photos");
-			
-			RGBLibrary rgbLib = new RGBLibrary(imglib.getLibrary());
-			
-			BufferedImage image = ImageIO.read(new File("anime.png"));
-			ImageGrid imgGrid = new ImageGrid(false, 8, 8, image);
-			
-			ImageTinder imgTinder = new ImageTinder(rgbLib.getRGBList(), imgGrid);
-			
-			MosaicBuilder mosaicBuilder = new MosaicBuilder(imglib, imgTinder.findMatches('R'));
-			mosaicBuilder.createMosaic();
-			
-			long endTime = System.currentTimeMillis() - startTime;
-			System.out.println(endTime);
-			
-			
-			
-		} catch (IOException e) {
+		ParaTask.init(4);
+		@Future
+		int task1 = taskOne();
+		
+		@Future
+		int task2 = taskTwo();
+	
+		@Future(depends = "task1, task2")
+		int task3 = taskThree(task1, task2);
+
+		
+	}
+	
+	public static int taskOne() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		
+		System.out.println(Thread.currentThread().getId());
+		return 1;
+	}
+	
+	public static int taskTwo() {
+		System.out.println(Thread.currentThread().getId());
+		return 2;
+	}
+	
+	public static int taskThree(int task1, int task2) {
+		System.out.println(Thread.currentThread().getId());
+		return task1 + task2;
 	}
 }
